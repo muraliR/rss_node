786bccacffdb3bb83673cc2c2eb482e04b2487aa
Rankit Ranjan <rankit@metabaze.com>
2017-02-02 16:34:02 +0530

    db changes

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 var Mongoose = require('mongoose');
-Mongoose.connect('mongodb://localhost/rss');
+Mongoose.connect("mongodb://rss:rss@ds139949.mlab.com:39949/rssndtv");
 var db = Mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
 db.once('open', function callback() {
